f1848e100d82adaef422cd73ebf57b765a135f6a
Shin Hyo Kyung <cdkrcd8@gmail.com>
2025-08-07 14:47:51 +0900

    init app

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css'; // App.css에 설정한 css가 현재 파일의 태그에 반영됨
+
+function App() {
+  return (
+    <div className="App">
+      <h1>안녕하세요</h1>
+    </div>
+  );
+}
+
+export default App;
